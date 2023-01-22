77f6a6284631891ed90f013206895497924a17e5
its julii <nanji828@gmail.com>
2023-01-22 12:16:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08.Cinema_Ticket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = Console.ReadLine();
+            int ticketPrice = 0;
+            if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Friday")
+            {
+                ticketPrice = 12;
+            }
+
+
+            else if (dayOfWeek == "Wednesday" || dayOfWeek == "Thursday")
+            {
+                ticketPrice = 14;
+            }
+            else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+            {
+                ticketPrice = 16;
+            }
+            Console.WriteLine(ticketPrice);
+        }
+    }
+}
